0bb9ccf2a27ef01a7719f75a53f06c697b0299de
Nathaniel Benguira <benguira@gmail.com>
2022-05-26 14:45:11 +0300

    init

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = (props) => {
+    const name = props.name;
+    const email = props.email;
+    const id = props.id;
+  return (  
+      <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+          <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+          <div>
+              <h2>{name}</h2>
+              <p>{email}</p>
+          </div>
+      </div>
+  )
+}
+
+export default Card;
